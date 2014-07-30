5ae106f7720674e51d2b754ede76cda16a88e7ba
Your Name <you@example.com>
2014-07-30 05:01:26 +0000

    Added Loops

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,43 @@ class ApplicationController < ActionController::Base
     render 'show'
   end
   
+  def new
+  end
+  
+  def create
+    p = Person.new
+    p.name = params['name']
+    p.title = params['title']
+    p.picture = params['picture']
+    p.text = params['text']
+    p.location = params['location']
+    p.save
+    redirect_to "/user/#{p.id}"
+  end
+  
+  
+  def edit
+    @p = Person.find_by_id(params['id'])
+  end
+  
+  def update
+    p = Person.find_by_id(params['id'])
+    p.name = params['name']
+    p.title = params ['title']
+    p.picture = params ['picture']
+    p.text = params ['text']
+    p.location = params ['location']
+    p.save
+    redirect_to "/user/#{p.id}"
+  end
+  
+  def destroy
+    p = Person.find_by_id(params['id'])
+    p.destroy
+  end
+  
+  def index
+    @p = Person.all
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  get '/rails/:id' => 'application#show'
+  #Creating
+  get '/new_user' => 'application#new'
+  get '/create_user' => 'application#create'
+  
+  #Reading
+  get '/user/:id' => 'application#show'
+  get '/users' => 'application#index'
+  
+  
+  #Updating
+  get '/user/:id/edit' => 'application#edit'
+  get'/updeate_user/:id' => 'application#update'
+  
+  #Deleting
+  get '/users/:id/destroy' => 'application#destroy'
+
+
 end
